80e89219ced6785ddfbd54834eed625878fbb3ac
mahdi sadeghi <mahdisadeghimsm1376@gmail.com>
2024-11-17 19:06:56 +0330

    complete overview and mainpage for test routing

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,7 +1,10 @@
 
 export default function Overview() {
     return (
-    <div>overview</div>
+      <>
+    <div className="flex justify-center mt-10 text-5xl">overview</div>
+    <div className="flex justify-center mt-10 text-lg"> please click dashboard in sidebar</div>
+    </>
     );
   }
   
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <>
+    <div className="flex justify-center mt-10 text-5xl"> main page</div>
+    <div className="flex justify-center mt-10 text-lg"> please click dashboard in sidebar</div>
+    </>
+    
   );
 }
